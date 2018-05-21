8423c2d99b6d3e86fdcf90a9c47028c03792bacd
lilygoose89 <32081648+lilygoose89@users.noreply.github.com>
2018-05-21 23:48:11 +0700

    started styling

--- main.js
+++ main.js
@@ -1,4 +1,45 @@
-$(document).ready(function(){
+$(document).ready(function() {
+
+	// Test for placeholder support
+    $.support.placeholder = (function(){
+        var i = document.createElement('input');
+        return 'placeholder' in i;
+    })();
+
+    // Hide labels by default if placeholders are supported
+    if($.support.placeholder) {
+        $('.form-label').each(function(){
+            $(this).addClass('js-hide-label');
+        });
+
+        // Code for adding/removing classes here
+        $('.form-group').find('input, textarea').on('keyup blur focus', function(e){
+
+            // Cache our selectors
+            var $this = $(this),
+                $parent = $this.parent().find("label");
+
+						switch(e.type) {
+							case 'keyup': {
+								 $parent.toggleClass('js-hide-label', $this.val() == '');
+							} break;
+							case 'blur': {
+								if( $this.val() == '' ) {
+                    $parent.addClass('js-hide-label');
+                } else {
+                    $parent.removeClass('js-hide-label').addClass('js-unhighlight-label');
+                }
+							} break;
+							case 'focus': {
+								if( $this.val() !== '' ) {
+                    $parent.removeClass('js-unhighlight-label');
+                }
+							} break;
+							default: break;
+						}
+        });
+    }
+
 
   // flexslider
   $('.flexslider').flexslider({
@@ -38,69 +79,7 @@ $(document).ready(function(){
 
   });
 
-  // form helper
-  $(document).ready(function() {
-  	// Test for placeholder support
-      $.support.placeholder = (function(){
-          var i = document.createElement('input');
-          return 'placeholder' in i;
-      })();
 
-      // Hide labels by default if placeholders are supported
-      if($.support.placeholder) {
-          $('.form-label').each(function(){
-              $(this).addClass('js-hide-label');
-          });
-
-          // Code for adding/removing classes here
-          $('.form-group').find('input, textarea').on('keyup blur focus', function(e){
-
-              // Cache our selectors
-              var $this = $(this),
-                  $parent = $this.parent().find("label");
-
-  						switch(e.type) {
-  							case 'keyup': {
-  								 $parent.toggleClass('js-hide-label', $this.val() == '');
-  							} break;
-  							case 'blur': {
-  								if( $this.val() == '' ) {
-                      $parent.addClass('js-hide-label');
-                  } else {
-                      $parent.removeClass('js-hide-label').addClass('js-unhighlight-label');
-                  }
-  							} break;
-  							case 'focus': {
-  								if( $this.val() !== '' ) {
-                      $parent.removeClass('js-unhighlight-label');
-                  }
-  							} break;
-  							default: break;
-  						}
-  						// previous implementation with ifs
-              /*if (e.type == 'keyup') {
-                  if( $this.val() == '' ) {
-                      $parent.addClass('js-hide-label');
-                  } else {
-                      $parent.removeClass('js-hide-label');
-                  }
-              }
-              else if (e.type == 'blur') {
-                  if( $this.val() == '' ) {
-                      $parent.addClass('js-hide-label');
-                  }
-                  else {
-                      $parent.removeClass('js-hide-label').addClass('js-unhighlight-label');
-                  }
-              }
-              else if (e.type == 'focus') {
-                  if( $this.val() !== '' ) {
-                      $parent.removeClass('js-unhighlight-label');
-                  }
-              }*/
-          });
-      }
-  });
 
   // smooth scrolling
   $("a[href^='#']").click(function () {
