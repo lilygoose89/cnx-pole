3b03c2489f3990920d0f511f321777f35c3f1aec
lilygoose89 <32081648+lilygoose89@users.noreply.github.com>
2018-05-22 15:51:47 +0700

    finish styling

--- main.js
+++ main.js
@@ -41,6 +41,14 @@ $(document).ready(function() {
     }
 
 
+  //click hamburger icon
+  $('.toggle-mobile-menu').click(function(){
+
+      //show mobile menu
+      $('#mobile-menu').toggleClass('mobile-menu-visible')
+  });
+
+
   // flexslider
   $('.flexslider').flexslider({
 
